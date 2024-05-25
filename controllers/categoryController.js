8f317d2194aa5a5185c9b325bd493339bccef37e
MatasNacevicius <matas.nacevicius@stud.kitm.lt>
2024-05-25 15:22:23 +0300

    pataisymai

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -41,14 +41,18 @@ const getAllCategories = asyncHandler(async (req, res) => {
 // Route: DELETE /api/categories/:id
 // Access: Private (assuming only admin can delete a category)
 const deleteCategory = asyncHandler(async (req, res) => {
-  const category = await Category.findById(req.params.id);
+  try {
+    const category = await Category.findById(req.params.id);
 
-  if (category) {
-    await category.remove();
+    if (!category) {
+      res.status(404).json({ message: "Category not found" });
+      return;
+    }
+
+    await Category.deleteOne({ _id: req.params.id });
     res.json({ message: "Category removed" });
-  } else {
-    res.status(404);
-    throw new Error("Category not found");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
